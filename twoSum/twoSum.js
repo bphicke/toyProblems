ff39d4623dfad2d50bc028ed0671802f6afd8d5d
brian <bphicke@gmail.com>
2018-09-14 13:29:25 -0500

    Faster Solution

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -1,12 +1,24 @@
 //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
+// function twoSum (array, target) {
+//     for (let i = 0; i < array.length; i++) {
+//         for (let j = i + 1; j < array.length; j++) {
+//             if (array[i] + array[j] === target) {
+//                 return [i, j];
+//             }
+//         }
+//     }
+// }
+
 function twoSum (array, target) {
+    let hash = {};
+    array.forEach((val, index)=>{
+        hash[val] = index;
+    });
     for (let i = 0; i < array.length; i++) {
-        for (let j = i + 1; j < array.length; j++) {
-            if (array[i] + array[j] === target) {
-                return [i, j];
-            }
+        if (hash[target - array[i]]) {
+            return [ i, hash[target - array[i]] ];
         }
     }
 }
