283049a4d2eeb9d39652275666acb91ebe094434
brian <bphicke@gmail.com>
2018-09-14 11:17:14 -0500

    Solve twoSum

---
+++ twoSum/twoSum.js
@@ -0,0 +1,15 @@
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+function twoSum (array, target) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9), ' Should equal [ 0, 1 ]');
+console.log(twoSum([31, 2, 3, 4, 5, 6, 7, 29], 31), ' Should equal [ 1, 7 ]');
